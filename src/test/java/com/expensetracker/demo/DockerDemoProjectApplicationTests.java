ae94af37c609586cbe6f41979a220449037b6d97
rakesh.kuchana <rakeshkuchana@hotmail.com>
2024-03-08 13:42:51 +0530

    first commit

---
+++ src/test/java/com/expensetracker/demo/DockerDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensetracker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
